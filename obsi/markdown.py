13d9c9c72d65936c9229e1e463e4a58c8a9b18c3
Karl Lorey <git@karllorey.com>
2021-05-10 13:16:20 +0200

    sort index notes by title (again)

--- obsi/markdown.py
+++ obsi/markdown.py
@@ -5,7 +5,9 @@ def create_index(title, notes):
     """creates an index of notes"""
     env = get_jinja_env()
     index_template = env.get_template("index.md")
-    return index_template.render(title=title, notes=notes)
+    return index_template.render(
+        title=title, notes=sorted(notes, key=lambda n: n.title)
+    )
 
 
 def get_jinja_env():
