d1b9ee4f31cc1d5cb7fa8841f123e0ed01abdabb
Karl Lorey <git@karllorey.com>
2021-05-10 12:35:56 +0200

    index generation as own function

--- cli.py
+++ cli.py
@@ -5,6 +5,8 @@ import click
 from obsi.markdown import create_index
 from obsi.storage import gen_notes
 
+NOTES_PATH = "/notes/"
+
 
 @click.group()
 def cli():
@@ -13,9 +15,14 @@ def cli():
 
 @cli.command()
 def run():
+    for index in generate_indexes():
+        print(index, "\n\n")
+
+
+def generate_indexes(untagged_index=True):
     notes_per_tag = defaultdict(set)
     notes_untagged = []
-    for note in gen_notes("/notes/"):
+    for note in gen_notes(NOTES_PATH):
         for tag in note.tags:
             notes_per_tag[tag].add(note)
 
@@ -27,9 +34,10 @@ def run():
         print()
 
     for tag, notes in notes_per_tag.items():
-        print(create_index(tag, notes), "\n\n")
+        yield create_index(tag, notes)
 
-    print(create_index("untagged notes", notes_untagged))
+    if untagged_index:
+        yield create_index("untagged notes", notes_untagged)
 
 
 if __name__ == "__main__":
